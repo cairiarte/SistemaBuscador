2329a6ffa836db50ab8517f93515353e2b41dd66
cairiarte <c.a.iriarte@gmail.com>
2021-09-01 00:41:37 -0400

    Cambios 0041

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        public string Usuario { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name="Contraseña")]
+        public string Password { get; set; }
+    }
+}
