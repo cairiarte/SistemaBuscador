d4ae4d5c4484772403c05e68dd809402c294b49f
cairiarte <c.a.iriarte@gmail.com>
2021-09-01 22:04:26 -0400

    Cambios 2 22.04

---
+++ Repositories/LoginRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Repositories
+{
+    public class LoginRepository
+    {
+        public bool UserExist(string usuario, string password)
+        {
+            bool result = false;
+            string connectionString = "server=localhost;database=SistemaBuscador;Integrated Security=true;";
+            using SqlConnection sql = new SqlConnection(connectionString);
+            using SqlCommand cmd = new SqlCommand("sp_check_use", sql);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@user", usuario));
+            cmd.Parameters.Add(new SqlParameter("@password", password));
+            sql.Open();
+
+            int bdresult = (int)cmd.ExecuteScalar();
+            if (bdresult > 0)
+            {
+                result = true; 
+            }
+            return result;
+        }
+    }
+}
